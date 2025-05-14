b6688a6086f12ed00156a2d3bbb3cc737936b63c
issamchlf <issamchellaf734@gmail.com>
2025-05-14 17:08:07 +0200

    feat: implement web scraping logic in scrapeQuotes method

--- app/Http/Controllers/webScrapingController.php
+++ app/Http/Controllers/webScrapingController.php
@@ -2,18 +2,29 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\BrowserKit\HttpBrowser;
 
 use Illuminate\Http\Request;
 
 class webScrapingController extends Controller
 {
     public function scrapeQuotes(): JsonResponse
+    {
 
-{
+    $browser = new HttpBrowser(HttpClient::create()); 
+    // This enables you to make HTTP requests by simulating browser behavior
 
-// scraping logic...
+    $crawler = $browser->request('GET', 'https://visita.malaga.eu/es/');
+    // This sends a GET request to the specified URL
 
-return response()->json('Hello, World!');
+    $html = $crawler->outerHtml();
+     // This retrieves the HTML content of the page
 
-}
+    $decodedHtml = html_entity_decode($html);
+     // This decodes any HTML entities in the HTML content
+     
+    return response()->json($decodedHtml); 
+    
+    }
 }
